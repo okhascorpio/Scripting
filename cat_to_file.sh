0393f7b2a82e5de0b55a76d2a7f4f5df57607976
okhascorpio <okhascorpio@gmail.com>
2021-10-08 08:58:28 +0300

    d3

---
+++ cat_to_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat > test_file_2.txt
+echo "Now append"
+cat >> test_file_2.txt
---
+++ commet_to_terminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<< comment
+echo "This will not echo"
+this is just a comment block
+comment
+
+cat << ToTerminal
+Hello Sir! 
+This is a test script
+ToTerminal
+
+echo "Run Test"
